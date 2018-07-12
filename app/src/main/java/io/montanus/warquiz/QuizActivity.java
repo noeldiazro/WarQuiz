d3c7595cc8672f659adc26c99934fff5e67e4432
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-07-12 15:21:29 +0200

    Base project with empty activity.

---
+++ app/src/main/java/io/montanus/warquiz/QuizActivity.java
@@ -0,0 +1,13 @@
+package io.montanus.warquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuizActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
