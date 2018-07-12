90a578dde47b1a95327299dff34bc92d9bce6dfc
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-07-12 17:54:28 +0200

    Question hashcode is shown on render.

--- app/src/main/java/io/montanus/warquiz/QuizActivity.java
+++ app/src/main/java/io/montanus/warquiz/QuizActivity.java
@@ -2,18 +2,30 @@ package io.montanus.warquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Collections;
 
 public class QuizActivity extends Activity implements View {
+    private final QuizPresenter quizPresenter;
+
+    public QuizActivity() {
+        final InMemoryRepository repository =
+                new InMemoryRepository(Collections.singletonList(new Question()));
+        quizPresenter = new QuizPresenter(repository, this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        quizPresenter.onRender();
     }
 
     @Override
     public void setQuestion(Question question) {
-        // TODO
+        final TextView questionText = findViewById(R.id.question_text);
+        questionText.setText(question.toString());
     }
 
     @Override
