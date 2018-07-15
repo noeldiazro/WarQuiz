0614038f4a04e2eb2115c4c848d8a19834818421
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-07-15 18:36:17 +0200

    Test class refactored to parameterized test pattern.

--- core/src/test/java/io/montanus/warquiz/test/CheckAnswerToQuestionTest.java
+++ core/src/test/java/io/montanus/warquiz/test/CheckAnswerToQuestionTest.java
@@ -1,6 +1,12 @@
 package io.montanus.warquiz.test;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Collection;
+import java.util.Collections;
 
 import io.montanus.warquiz.Answer;
 import io.montanus.warquiz.Question;
@@ -8,10 +14,28 @@ import io.montanus.warquiz.Result;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public final class CheckAnswerToQuestionTest {
+
+    private final Answer correctAnswer;
+    private final Result expectedResult;
+    private final Answer userAnswer;
+
+    public CheckAnswerToQuestionTest(
+            Answer correctAnswer, Answer userAnswer, Result expectedResult) {
+        this.correctAnswer = correctAnswer;
+        this.userAnswer = userAnswer;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameters(name = "Correct answer is {0} and user responds {1}, then result is {2}")
+    public static Collection<Object[]> parameters() {
+        return Collections.singletonList(new Object[] {Answer.TRUE, Answer.TRUE, Result.RIGHT});
+    }
+
     @Test
     public void bothCorrectAndUserAnswerAreTrue() {
-        final Question question = new Question("::irrelevant::", Answer.TRUE);
-        assertEquals(Result.RIGHT, question.check(Answer.TRUE));
+        final Question question = new Question("::irrelevant::", correctAnswer);
+        assertEquals(expectedResult, question.check(userAnswer));
     }
 }
